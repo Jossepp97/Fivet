777b49cfd41f9907d26278c5de27e70d5afef1cd
José Ávalos <josseavalos97@gmail.com>
2022-04-25 23:37:31 -0400

    Initial Commit

---
+++ src/main/java/cl/ucn/disc/pdis/fivet/model/Persona.java
@@ -0,0 +1,65 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 José Ávalos Guzmán
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package cl.ucn.disc.pdis.fivet.model;
+import cl.ucn.disc.pdis.fivet.orm.BaseEntity;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * The Persona.
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@DatabaseTable
+public final class Persona extends BaseEntity {
+
+    /**
+     * The Rut.
+     */
+    @Getter
+    @DatabaseField(canBeNull = false, unique = true)
+    private String rut;
+
+    /**
+     * The Nombre
+     */
+    @Getter
+    @DatabaseField(canBeNull = false)
+    private String nombre;
+
+    /**
+     * The Email
+     */
+    @Getter
+    @DatabaseField(canBeNull = false, unique = true)
+    private String email;
+
+
+}
