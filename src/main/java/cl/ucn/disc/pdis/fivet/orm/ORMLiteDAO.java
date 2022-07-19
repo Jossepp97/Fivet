f2356c13d8963c6a9676e0633e66bdaf4f0236ef
José Ávalos <josseavalos97@gmail.com>
2022-07-19 05:31:42 -0400

    fix(project): update and checkstyle

--- src/main/java/cl/ucn/disc/pdis/fivet/orm/ORMLiteDAO.java
+++ src/main/java/cl/ucn/disc/pdis/fivet/orm/ORMLiteDAO.java
@@ -10,8 +10,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -24,13 +24,19 @@
 
 package cl.ucn.disc.pdis.fivet.orm;
 
+import com.google.common.collect.Lists;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.field.DataPersisterManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
 import lombok.NonNull;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.SQLException;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,9 +44,17 @@ import java.util.Optional;
 /**
  * The ORMLite implementation of DAO.
  *
- * @author Jose Avalos Guzman
+ * @author Jose Avalos Guzman.
  */
+@Slf4j
 public final class ORMLiteDAO<T extends BaseEntity> implements DAO<T> {
+
+    static {
+        log.debug("Registering the ZonedDateTimeType...");
+        DataPersisterManager.registerDataPersisters(ZonedDateTimeType.INSTANCE);
+        log.debug("Registering the LocalDateType...");
+        DataPersisterManager.registerDataPersisters(LocalDateType.INSTANCE);
+    }
 
     /**
      * The real DAO (connection to ORMLite DAO).
@@ -48,89 +62,156 @@ public final class ORMLiteDAO<T extends BaseEntity> implements DAO<T> {
     private final Dao<T, Integer> theDao;
 
     /**
-     * The Constructor of ORMLiteDAO
+     * The ConnectionSource
+     */
+    private static ConnectionSource cs;
+
+    /**
+     * The Constructor of ORMLiteDAO.
      * @param cs        the connection to the database.
      * @param clazz     the type of T.
      */
     @SneakyThrows(SQLException.class)
-    public ORMLiteDAO(@NonNull final ConnectionSource cs, @NonNull Class<T> clazz){
-        this.theDao = DaoManager.createDao(cs,clazz);
+    public ORMLiteDAO(@NonNull final ConnectionSource cs,
+                      @NonNull Class<T> clazz) {
+        this.theDao = DaoManager.createDao(cs, clazz);
+        ORMLiteDAO.cs = cs;
     }
 
+    /**
+     * Build a ConnectionSource to use.
+     * @param s the database url
+     * @return the ConnectionSource
+     */
+    @SneakyThrows
+    public static ConnectionSource buildConnectionSource(String s) {
+        cs = new JdbcConnectionSource(s);
+        return cs;
+    }
 
     /**
-     * Get optional T
+     * Get optional T.
      *
-     * @param id to search
+     * @param id to search.
      */
     @SneakyThrows(SQLException.class)
     @Override
-    public Optional<T> get(final Integer id) {
+    public Optional<T> get(Integer id) {
 
         // Exec the SQL
         T t = this.theDao.queryForId(id);
-        return t == null ? Optional.empty() : Optional.of(t);
-
-        /*  Lo mismo que la linea 72
-
-        if( t== null){
+        if (t == null) {
             return Optional.empty();
-        }else{
-            return Optional.of(t);
         }
-         */
+        if (t.getDeletedAt() != null) {
+            return Optional.empty();
+        }
+        return Optional.of(t);
+
     }
 
     /**
-     * Get all the Ts
+     * Get optional t.
+     * @param attrib name of attribute.
+     * @param value the value.
+     * @return T.
+     */
+    @SneakyThrows
+    @Override
+    public Optional<T> get(final String attrib, final Object value) {
+        List<T> list = this.theDao.queryForEq(attrib, value);
+
+        for (T t : list) {
+            if (t.getDeletedAt() == null) {
+                return Optional.of(t);
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Get all the Ts.
+     * @return the List of T.
      *
-     * @return the List of T
      */
     @SneakyThrows(SQLException.class)
     @Override
     public List<T> getAll() {
-        return this.theDao.queryForAll();
+        List<T> list = Lists.newArrayList();
+
+        for (T t : this.theDao.queryForAll()) {
+            if (t.getDeletedAt() == null) {
+                list.add(t);
+            }
+        }
+        return list;
     }
 
     /**
      * Save a T.
      *
-     * @param t to save
+     * @param t to save.
      */
     @SneakyThrows(SQLException.class)
     @Override
     public void save(T t) {
         int created = this.theDao.create(t);
 
-        if (created != 1){
-            throw new SQLException("Rows created =/= 1");
+        if (created != 1) {
+            throw new SQLException("Rows created != 1");
         }
     }
 
     /**
-     * Delete a T
+     * Delete a T.
      *
-     * @param t to delete
+     * @param t to delete.
      */
     @SneakyThrows(SQLException.class)
     @Override
     public void delete(T t) {
-        this.delete(t.getId());
+        boolean exist = this.theDao.idExists(t.getId());
+        if (!exist) {
+            log.warn("T object not found");
+        } else {
+        t.deletedAt = ZonedDateTime.now();
+            if (this.theDao.update(t) != 1) {
+                throw new SQLException("Rows updated != 1");
+            }
+        }
     }
 
-
-
      /**
-     * Delete a T with id
+     * Delete a T with id.
      *
      * @param id of th et to delete.
      */
     @SneakyThrows(SQLException.class)
     @Override
-    public void delete(Integer id) {
-        int deleted = this.theDao.deleteById(id);
-        if (deleted != 1){
-            throw new SQLException("Rows deleted =/= 1");
+    public void delete(final Integer id) {
+        boolean exist = this.theDao.idExists(id);
+        if (!exist) {
+            throw new SQLException("The entity with id: {} not exists.", String.valueOf(id));
+        } else {
+            Optional<T> t = this.get(id);
+
+            if (t.isEmpty()) {
+                log.warn("Entity t {} not found", id);
+                return;
+            }
+            t.get().deletedAt = ZonedDateTime.now();
+            if (this.theDao.update(t.get()) != 1) {
+                throw new SQLException("Rows deleted != 1");
+            }
         }
+    }
+    /**
+     * Drop and Create Table
+     */
+    @Override
+    @SneakyThrows
+    public void dropAndCreateTable() {
+        TableUtils.dropTable(cs, theDao.getDataClass(), true);
+        TableUtils.createTable(cs, theDao.getDataClass());
     }
 }
